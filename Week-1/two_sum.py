7c21ee100e91605c0ea346b33bca9e0c032767c4
santhoshivan23 <santhoshivan23@gmail.com>
2024-09-23 10:51:01 +0530

    two sum

---
+++ Week-1/two_sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        visited = {}
+        for idx, num in enumerate(nums):
+            if target - num in visited:
+                return [visited[target-num], idx]
+            visited[num] = idx
+        return []
+
+# https://leetcode.com/problems/two-sum/
